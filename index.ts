6f21778eb883a939bc4893af56561cd2f5489ad6
Cyrus28214 <cyrus@gmail.com>
2025-01-27 12:48:52 +0800

    feat: getTweetDetail

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import fs from "fs/promises";
 
 import { getUserByScreenName } from "./api/getUserByScreenName";
 import { getUserTweets } from "./api/getUserTweets";
+import { getTweetDetail } from "./api/getTweetDetail";
 
 let res = await getUserByScreenName("elonmusk");
 res = JSON.stringify(res, null, 2);
@@ -12,3 +13,8 @@ res = await getUserTweets("44196397", 20);
 res = JSON.stringify(res, null, 2);
 console.log(res);
 // await fs.writeFile("examples/UserTweets.json", res);
+
+res = await getTweetDetail("1883416606933680469");
+res = JSON.stringify(res, null, 2);
+console.log(res);
+await fs.writeFile("examples/TweetDetail.json", res);
